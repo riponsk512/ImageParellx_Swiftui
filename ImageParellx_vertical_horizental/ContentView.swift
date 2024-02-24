ca74fdae8920275c1f739ae57e7422e2e387de3f
Ripon Seikh <34369083+riponsk512@users.noreply.github.com>
2024-02-24 21:46:54 +0530

    Initial Commit

---
+++ ImageParellx_vertical_horizental/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageParellx_vertical_horizental
+//
+//  Created by Ripon sk on 24/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageParellx_vertical_horizental/ImageParellx_vertical_horizentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageParellx_vertical_horizentalApp.swift
+//  ImageParellx_vertical_horizental
+//
+//  Created by Ripon sk on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageParellx_vertical_horizentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
